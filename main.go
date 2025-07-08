b59ebc5129806ece22dddeb221603faadec8a03a
felix-kuang <felixshi26@gmail.com>
2025-07-08 15:09:07 +0700

    fix: setup deploy

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/Felix-kuang/todo-golang-api/db"
 	"github.com/Felix-kuang/todo-golang-api/routes"
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
 
@@ -11,12 +15,17 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// func init() {
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		log.Fatal("Error loading .env file")
-// 	}
-// }
+func init() {
+	if os.Getenv("RAILWAY_ENVIRONMENT_ID") == "" {
+		godotenv.Load()
+	} else {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+}
 
 func main() {
 	db.Connect()
